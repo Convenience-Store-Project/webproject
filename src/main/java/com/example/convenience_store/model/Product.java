59d5c43ef0fd81c8cb6eb7433f1c06637f76d2e0
kangminkim <lnewgatel@gmail.com>
2023-08-28 16:02:12 +0900

    controller setting

---
+++ src/main/java/com/example/convenience_store/model/Product.java
@@ -0,0 +1,4 @@
+package com.example.convenience_store.model;
+
+public class Product {
+}
---
+++ src/main/java/com/example/convenience_store/repository/CustomerRepository.java
@@ -0,0 +1,4 @@
+package com.example.convenience_store.repository;
+
+public interface CustomerRepository {
+}
