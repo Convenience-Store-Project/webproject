0c4843aa2f91484922bae1184d77dc6f6a0dd8ca
mstny <msj6872@gmail.com>
2023-09-12 14:05:11 +0900

    latest

---
+++ src/main/java/com/example/convenience_store/controller/ReservationController.java
@@ -0,0 +1,31 @@
+package com.example.convenience_store.controller;
+
+import com.example.convenience_store.model.entity.Customer;
+import com.example.convenience_store.model.entity.Reservation;
+import com.example.convenience_store.service.ProductService;
+import com.example.convenience_store.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+public class ReservationController {
+    @Autowired
+    private ReservationService reservationService;
+    @GetMapping("/mypage")
+    public String showReservations(Model model) {
+        List<Reservation> reservations = reservationService.getAllReservations();
+        model.addAttribute("reservations", reservations);
+
+        return "mypage";
+    }
+
+
+
+}
